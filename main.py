b16ef1501c7247407bff424129ab1a9a7594ad05
dtjpro <dtjpro.social@gmail.com>
2025-04-23 00:06:22 +0200

    start

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+key = 2
+
+input1 = "hi"
+
+string_Array = []
+
+for i in input1:
+    string_Array.append(i)
+
+print(string_Array)
---
+++ test.py
@@ -0,0 +1,8 @@
+def int_to_char(inp):
+    chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'v', 'x', 'y', 'z']
+    inp -= 1
+    return chars[inp]
+
+print(int_to_char(1))
+
+
